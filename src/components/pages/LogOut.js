6cab0c80b6518eefbbf9fc9a2656890317286c63
Akerele, Tosin James <akerele.james1699@gmail.com>
2021-06-09 23:28:07 +0100

    Delete src directory

--- src/components/pages/LogOut.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LogOut() {
-  return (
-    <>
-      <h1 className='logout'>LOG OUT</h1>
-    </>
-  );
-}
--- src/components/pages/Tosin.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import '../../App.css'
-
-export default function Tosin () {
-  return (<h1 className='tosin'>TOSIN</h1>)
-}
